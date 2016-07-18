3509ab86767ed63797661a6bf8cd7749929dfe22
mbdaks <m.b.daks@gmail.com>
2016-07-18 17:04:09 +0530

    Formatting changes and Functions reshuffled to the required classes
    Formatting done(the spaces and = and , signs spaces)
    Appropriate names given to the functions and classes
    Functions are now in right classes as suggested before

--- src/finalAutomationFramework/SeleniumActionFunctions.java
+++ src/finalAutomationFramework/SeleniumActionFunctions.java
@@ -1,38 +1,39 @@
 package finalAutomationFramework;
 
 import java.io.IOException;
-
+import java.util.Calendar;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-
 import org.openqa.selenium.support.ui.Select;
 
 public class SeleniumActionFunctions {
+	public static int currentRowInDataExcel=0;
 	
-	public static int currentRowInDataExcel=1;
-
-	public void selectAction(Data dataObject, String pageName, String controlHead) throws IOException{
-		String locatorValue=IndependentAutomationFunctions.returnLocatorValue(dataObject, pageName,controlHead);
-		String sendValue=IndependentAutomationFunctions.returnValueToBeSentToWebPage(dataObject,currentRowInDataExcel,controlHead);
+	public static void openWebsite(WebsiteSpecificData dataObject){
+		long startTime = Calendar.getInstance().getTimeInMillis();
+		dataObject.driver.manage().deleteAllCookies();
+		dataObject.driver.get(dataObject.baseUrl);
+		dataObject.driver.manage().window().maximize();
+		System.out.println("TIME TAKEN: " + (Calendar.getInstance().getTimeInMillis() - startTime));
+	}
+	
+	public void selectAction(WebsiteSpecificData dataObject, String pageName, String controlHead) throws IOException{
+		String locatorValue = IndependentAutomationFunctions.returnLocatorValue(dataObject, pageName, controlHead);
+		String sendValue = IndependentAutomationFunctions.returnValueToBeSentToWebPage(dataObject, currentRowInDataExcel, controlHead);
 		Select adult = new Select(dataObject.driver.findElement(By.id(locatorValue)));
-		adult.selectByVisibleText(sendValue);
-		System.out.println(locatorValue+"\t"+sendValue);
-	
+		adult.selectByVisibleText(sendValue);	
 	}
 
-	public void sendKeysAction(Data dataObject,String pageName,String controlHead) throws IOException{
-		String locatorValue=IndependentAutomationFunctions.returnLocatorValue(dataObject, pageName,controlHead);
-		String sendValue=IndependentAutomationFunctions.returnValueToBeSentToWebPage(dataObject,currentRowInDataExcel,controlHead);
+	public void sendKeysAction(WebsiteSpecificData dataObject, String pageName, String controlHead) throws IOException{
+		String locatorValue=IndependentAutomationFunctions.returnLocatorValue(dataObject, pageName, controlHead);
+		String sendValue=IndependentAutomationFunctions.returnValueToBeSentToWebPage(dataObject, currentRowInDataExcel, controlHead);
 		dataObject.driver.findElement(By.id(locatorValue)).clear();
 		dataObject.driver.findElement(By.id(locatorValue)).sendKeys(sendValue);
 		dataObject.kb.pressKey(Keys.RETURN);
-
 	}
 
-	public void clickAction(Data dataObject,String pageName,String controlHead) throws IOException{
-		String locatorValue=IndependentAutomationFunctions.returnLocatorValue(dataObject,pageName,controlHead);
+	public void clickAction(WebsiteSpecificData dataObject, String pageName, String controlHead) throws IOException{
+		String locatorValue = IndependentAutomationFunctions.returnLocatorValue(dataObject, pageName, controlHead);
 		dataObject.driver.findElement(By.id(locatorValue)).click();
-
 	}
-	
 }
---
+++ src/finalAutomationFramework/WebsiteSpecificData.java
@@ -0,0 +1,52 @@
+package finalAutomationFramework;
+
+import java.io.IOException;
+import java.util.HashMap;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Keyboard;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class WebsiteSpecificData {
+	String baseUrl;
+	String propertyFile,dataFile,resultFile;
+	int testDataNum;
+	Workbook dataWorkBook,propertyWorkBook,resultWorkBook;
+	Sheet dataSheet,propertySheet,resultSheet;
+	String screenShotBase;
+	String host,port,mailFrom,password,mailTo,subject,message;
+	HashMap<String, Object[]> testresultdata;
+	WebDriver driver;
+	JavascriptExecutor jse;
+	Keyboard kb;
+
+	WebsiteSpecificData() throws IOException, InvalidFormatException
+	{
+		baseUrl = "http://uat.jetstar.com/SkySalesTest/Search.aspx";
+		propertyFile = "F:\\GIT\\finalAutomationFramework\\resources\\input\\Properties.xlsx";
+		dataFile = "F:\\GIT\\finalAutomationFramework\\resources\\input\\Data.xlsx";
+		resultFile = "F:\\GIT\\finalAutomationFramework\\resources\\output\\results\\ResultSheet_";
+		testDataNum = 0;
+		dataWorkBook = SupportingFunctions.returnWorkBook(dataFile);
+		dataSheet = SupportingFunctions.returnSheet(dataWorkBook);
+		propertyWorkBook = SupportingFunctions.returnWorkBook(propertyFile);
+		propertySheet = SupportingFunctions.returnSheet(propertyWorkBook);
+		screenShotBase = "F:\\GIT\\finalAutomationFramework\\resources\\output\\ScreenShots\\SS_";
+		host = "smtp.gmail.com";
+	    port = "25";
+	    mailFrom = "ykwsnape321@gmail.com";
+	    password = "DAWNDYNASTYDIARIES";
+	    mailTo = "ykw132snape@gmail.com";
+	    subject = "Execution Results : ";
+	    message = "Attached : excel sheet containing result";
+	    testresultdata = new HashMap<String, Object[]>();
+	    driver = new FirefoxDriver();
+		jse = (JavascriptExecutor)driver;
+		kb = ((RemoteWebDriver) driver).getKeyboard();
+	}
+
+}
